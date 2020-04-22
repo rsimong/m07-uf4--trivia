81fc91ca90c35cc72ab340459a6d953b03d101f6
M.Cortez95 <donar.sang.salle2019@gmail.com>
2020-04-22 23:22:59 +0200

    Login 90% terminado, falta compara la contraseña enviada con la encriptada

--- routes/triviaRoutes.js
+++ routes/triviaRoutes.js
@@ -5,10 +5,16 @@ const joiSchemaValidation = require('../Middlewares/joiSchemaValidation');
 const constants = require('../config/constants');
 const userSchema = require('../models/joi/userSchema');
 
-/* Register user */
+        /* Register user */
 router.post('/register',
     joiSchemaValidation.validate(userSchema.registerUser, constants.requestObj.BODY_PARAMS),
     userController.register
+);
+
+        /* Login */
+router.post('/login',
+    joiSchemaValidation.validate(userSchema.authenticateSchema, constants.requestObj.BODY_PARAMS),
+    userController.authenticate
 );
 
 module.exports = router;
