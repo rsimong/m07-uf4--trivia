69a4e51927057fc8466e17b0746fa3ad4a294175
Ricard SG <rsimon@mariabarcelona.com>
2020-05-05 18:19:03 +0200

    Create Joi Schemas

--- models/joi/searchesSchema.js
+++ models/joi/searchesSchema.js
@@ -1,10 +1,19 @@
 'use strict'
 const Joi = require('@hapi/joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 module.exports = {
-    queryParams: Joi.object({
-        date_from: Joi.date(),
-        date_to: Joi.date()
+    uriParams: Joi.object({
+        from: Joi.string().pattern(/(?:\d{4}-\d{2}-\d{2}|\d{4}\d{2}\d{2})/),
+        to: Joi.string().pattern(/(?:\d{4}-\d{2}-\d{2}|\d{4}\d{2}\d{2})/)
     })
-        .with('date_to', 'date_from')
+        .with('to', 'from'),
+
+    uriSearchId: Joi.object({
+        id: Joi.objectId().required()
+    }),
+
+    updateSearch: Joi.object({
+        owner: Joi.objectId()
+    })
 }
