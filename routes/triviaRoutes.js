4178c3660f2203ed7616757460609d96b2cd861f
Ricard SG <rsimon@mariabarcelona.com>
2020-05-13 19:06:38 +0200

    Last changes

--- routes/triviaRoutes.js
+++ routes/triviaRoutes.js
@@ -8,50 +8,43 @@ const userSchema = require('../models/joi/userSchema');
 const questionSchema = require('../models/joi/questionSchema');
 const tokenValidation = require('../middlewares/tokenValidation')
 
-        /* Register user */
+/* Register user */
 router.post('/register',
     joiSchemaValidation.validate(userSchema.registerUser, constants.requestObj.BODY_PARAMS),
     userController.register
 );
 
-        /* Login */
+/* Login */
 router.post('/login',
     joiSchemaValidation.validate(userSchema.authenticateSchema, constants.requestObj.BODY_PARAMS),
     userController.authenticate
 );
 
-        /*Questions CRUD*/
+/*Questions CRUD*/
 
-router.get('/', 
-    tokenValidation.validate,
+router.get('/',
+    tokenValidation.validate(),
     quetionController.getAllQuestions
 )
 
-router.post('/create', 
+router.post('/create',
     joiSchemaValidation.validate(questionSchema.createQuestion, constants.requestObj.BODY_PARAMS),
-    tokenValidation.validate,
+    tokenValidation.validate(),
     quetionController.creaetQuestion
 );
 
-router.put('/:id', 
+router.put('/:id',
     joiSchemaValidation.validate(questionSchema.questionIdSchema, constants.requestObj.PATH_PARAMS),
     joiSchemaValidation.validate(questionSchema.updateQuestion, constants.requestObj.BODY_PARAMS),
-    tokenValidation.validate,
+    tokenValidation.validate(),
     quetionController.updateQuestion
 );
 
 router.delete('/:id',
     joiSchemaValidation.validate(questionSchema.questionIdSchema, constants.requestObj.PATH_PARAMS),
-    tokenValidation.validate,
+    tokenValidation.validate(),
     quetionController.destroy
-    
-)
 
-        /**Test */
-router.get('/saludo', tokenValidation.validate,
-    (request,response)=>{
-        return response.send('Hola Oriol');
-    }
 )
 
 module.exports = router;
